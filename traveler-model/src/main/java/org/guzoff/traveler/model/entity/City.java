54403efa0652745bdc7a3e42911b5c0ce38f08fe
guzoff <guzoff@gmail.com>
2017-07-05 11:49:26 +0300

    Add tests for City, removeStation()

--- traveler-model/src/main/java/org/guzoff/traveler/model/entity/City.java
+++ traveler-model/src/main/java/org/guzoff/traveler/model/entity/City.java
@@ -1,6 +1,7 @@
 package org.guzoff.traveler.model.entity;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,16 +14,24 @@ public class City extends AbstractEntity {
     @Getter @Setter private String region;      //top level in the country
     private Set<Station> stations;
 
-    void addStation(Station station) {
+    public void addStation(final Station station) {
+        Objects.requireNonNull(station, "station parameter is not initialized");
         if (stations == null) {
-            stations = new HashSet();
+            stations = new HashSet<>();
         }
         stations.add(station);
         station.setCity(this);
     }
 
+    public void removeStation(final Station station) {
+        if (stations != null) {
+            stations.remove(station);
+        }
+    }
+    
     public Set<Station> getStations() {
         return CommonUtil.getSafeSet(stations);
     }
+
     
 }
--- traveler-model/src/test/java/org/guzoff/traveler/model/entity/CityTest.java
+++ traveler-model/src/test/java/org/guzoff/traveler/model/entity/CityTest.java
@@ -1,17 +1,60 @@
 package org.guzoff.traveler.model.entity;
 
+import java.util.Set;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CityTest {
     
+    private City city;
+    private Station station;
+    
+    @Before
+    public void setup() {
+        city = new City();
+        station = new Station();
+    }
+    
     @Test
     public void testAddValidStationSuccess() {
-        Station station = new Station();
-        City city = new City();
         city.addStation(station);
         assertTrue(containsStation(city, station));
         assertEquals(city, station.getCity());
+    }
+    
+    @Test
+    public void testRemoveValidStationSuccess() {
+        city.addStation(station);
+        city.removeStation(station);
+        assertFalse(containsStation(city, station));
+    }
+    
+    @Test
+    public void testRemoveStationFromEmptyStationsSuccess() {
+        city.removeStation(station);
+        assertFalse(containsStation(city, station));
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testAddNullStationFailure() {
+        city.addStation(null);
+        assertTrue(false);
+    }
+    
+    @Test
+    public void testRemoveNullStationResultsNoChanges() {
+        city.addStation(station);
+        Set stations = city.getStations();
+        city.removeStation(null);
+        assertEquals(stations, city.getStations());
+    }
+    
+    @Test
+    public void testAddDuplicateStationFailure() {
+        city.addStation(station);
+        city.addStation(station);
+        assertEquals(city.getStations().size(), 1);
     }
 
     private boolean containsStation(City city, Station station) {
